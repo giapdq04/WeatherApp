e06d62745897bf562a48df3f78e1607cb3b0cf9f
giapdq04 <167546581+giapdq04@users.noreply.github.com>
2024-08-18 16:09:41 +0700

    xóa lịch sử file weather.js

---
+++ src/api/weather.js
@@ -0,0 +1,31 @@
+
+// weatherapi.com
+import axios from "axios"
+import { apiKey } from "./api"
+const forecastEndpoint = params => `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityName}&days=${params.days}`
+
+const locationsEndpoint = params => `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${params.cityName}`
+
+const apiCall = async endpoint => {
+    const options = {
+        method: 'GET',
+        url: endpoint,
+    }
+
+    try {
+        const response = await axios.request(options);
+        return response.data;
+    } catch (error) {
+        console.error('error', error);
+        return null;
+    }
+}
+
+export const fetchWeatherForecast = params => {
+    return apiCall(forecastEndpoint(params))
+}
+
+export const fetchLocations = params => {
+    return apiCall(locationsEndpoint(params))
+}
+
